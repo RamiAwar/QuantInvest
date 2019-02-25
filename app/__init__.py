e883bdeff7609008b88e610f3a48174055ec85e5
Rami Awar <rami.awar.ra@gmail.com>
2019-02-25 17:35:18 +0100

    Added login forms with validators

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask 
+import os
 
 app = Flask(__name__);
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+
 
 # Workaround to circular imports! EW!
 from app import routes
+
 
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,26 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
 
-	user = {'username': 'Rami'}
+	user = {'username': 'Test'}
 
 	return render_template('index.html', title="Home", user=user);
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	
+	login_form = LoginForm()
+
+	if login_form.validate_on_submit():
+        
+		flash('Login requested for user with email {}, remember_me={}'.format(login_form.email.data, login_form.remember_me.data))
+		return redirect('/index')
+
+	return render_template('login.html', title="Login", form=login_form);
+
+
