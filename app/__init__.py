45cba4f4ef7bf481490f50516f40d9a2b7e7ee2c
Rami Awar <rami.awar.ra@gmail.com>
2019-02-26 09:48:36 +0100

    Included .env in .gitignore, added some database configuration

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,16 @@ import os
 app = Flask(__name__);
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 # Workaround to circular imports! EW!
-from app import routes
+from app import routes, models
 
 
 
--- config.py
+++ config.py
@@ -3,6 +3,13 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+    
+    # Disables feature of sql alchemy which notifies app every time change is about to be made
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 	DEBUG = False
 	TESTING = False
 	CSRF_ENABLED = True
