3d7d31d9f3a00c2c2aef0d5aabe4e5f3b91a7f10
Rami <rami.awar.ra@gmail.com>
2019-02-28 22:45:09 +0100

    Fixed indentation error.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app
 from app.forms import LoginForm
-
+from app.models import User
 from flask_login import current_user, login_user
 
 @app.route('/')
@@ -17,19 +17,35 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 
+
+	# TODO: Hide case by hiding Login redirection buttons for authenticated users : priority (1)
 	if current_user.is_authenticated:
 		return redirect(url_for('index'))
 	
 	login_form = LoginForm()
-
 	if login_form.validate_on_submit():
-        
-		flash('Login requested for user with email {}, remember_me={}'.format(login_form.email.data, login_form.remember_me.data))
+
+		# Fetch user
+		user = User.objects(username__exact=login_form.username.data).first()
+
+		# Check username and password correctness
+		if user is None or not user.check_password(form.password.data):
+
+			flash("Invalid username or password");
+			return redirect(url_for('login'));
+
+		login_user(user, remember=login_form.remember_me.data)
 		return redirect(url_for('index'))
+
 
 	return render_template('login.html', title="Login", form=login_form);
 
 
+@app.route('/logout')
+def logout():
+
+	logout_user();
+	return redirect(url_for('index'));
 
 
 
