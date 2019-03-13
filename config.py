523cb3a8fcdc8f61e71d17765defcda30bf0c6fd
Rami Awar <rami.awar.ra@gmail.com>
2019-03-13 15:54:33 +0100

    Refactored authentication and error page handling into Flask Blueprints.

--- config.py
+++ config.py
@@ -12,6 +12,14 @@ class Config(object):
 	MONGODB_URI = os.environ.get('MONGODB_URI') or \
 		'localhost:27017';
 
+	# TODO: Correct setup : priority (2)
+	# MAIL_SERVER = os.environ.get('MAIL_SERVER')
+ #    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+ #    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+ #    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+ #    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+ #    ADMINS = ['your-email@example.com']
+
 class ProductionConfig(Config):
 	DEBUG=False
 
