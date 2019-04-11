f62d24c6445b1748cff62df89ca1a81756ddcd70
Rami <rami.awar.ra@gmail.com>
2019-04-11 21:49:49 +0200

    Changed scheduler interval

--- entry.py
+++ entry.py
@@ -10,8 +10,10 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from startup_tasks import cache_data
 from datetime import datetime
 
+print("TEST)")
+
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=cache_data, trigger="interval", seconds=10)
+scheduler.add_job(func=cache_data, trigger="interval", days=1)
 scheduler.start()
 for job in scheduler.get_jobs():
     job.modify(next_run_time=datetime.now())
