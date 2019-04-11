bfa19cb17e4dd3da14c8376dbb706b2fcc1ad8ba
naderalawar <naderalawar@gmail.com>
2019-04-11 19:38:59 +0300

    scheduler now enqueues on startup

--- entry.py
+++ entry.py
@@ -8,7 +8,10 @@ snp500_tickers.initialize()
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from startup_tasks import cache_data
+from datetime import datetime
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=cache_data, trigger="interval", seconds=10)
 scheduler.start()
+for job in scheduler.get_jobs():
+    job.modify(next_run_time=datetime.now())
--- startup_tasks.py
+++ startup_tasks.py
@@ -4,10 +4,8 @@ from app.api.stock_fetcher.launch_task import launch_task
 
 def cache_data():
 
-    if StockDailyPrice.objects.first() == None:  # check if any data for any snp 500 stock exists
+    snp_500_tickers = snp500_tickers.objects.all()
+    snp_500_tickers = [ticker.symbol for ticker in snp_500_tickers]
 
-        snp_500_tickers = snp500_tickers.objects.all()
-        snp_500_tickers = [ticker.symbol for ticker in snp_500_tickers]
-
-        for i in range(0, len(snp_500_tickers), 100):
-            task = launch_task('fetch_snp500_data', list(snp_500_tickers[i:i + 100]))
+    for i in range(0, len(snp_500_tickers), 100):
+        task = launch_task('fetch_snp500_data', list(snp_500_tickers[i:i + 100]))
