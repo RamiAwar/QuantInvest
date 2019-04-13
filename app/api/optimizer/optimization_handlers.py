c97c4226a22196dae814089d1c6966345b366dc9
Rami <rami.awar.ra@gmail.com>
2019-04-13 17:32:51 +0200

    Added longer job timeout to launch_task jobs
    Backend jobs can now take several days time, but frontend jobs still have the default limit of 120s which is what is expected. Users are not expected to wait for more than 2 minutes.

--- app/api/optimizer/optimization_handlers.py
+++ app/api/optimizer/optimization_handlers.py
@@ -17,7 +17,7 @@ def max_sharpe(parameters):
     print("From optimizer: ", parameters)
 
     stocks_df = get_data(parameters["ticker_list"], datetime.strptime(parameters["start_date"], '%Y-%m-%d'),
-                         datetime.strptime(parameters["end_date"], '%Y-%m-%d'))
+       datetime.strptime(parameters["end_date"], '%Y-%m-%d'))
 
     print(stocks_df.head())
 
@@ -39,7 +39,7 @@ def max_sharpe(parameters):
     # backtest_results = get_daily_returns(cleaned_weights, parameters["start_date"], parameters["end_date"])
 
     backtest_results = backtest_portfolio(prices_df=stocks_df, portfolio=cleaned_weights, initial_amount=int(parameters[
-                                          "initial_amount"]), start_date=parameters["start_date"], end_date=parameters["end_date"])
+      "initial_amount"]), start_date=parameters["start_date"], end_date=parameters["end_date"])
 
     print(backtest_results.describe())
 
@@ -47,17 +47,17 @@ def max_sharpe(parameters):
 
     output = {
 
-        "weights": {
-            "labels": list(cleaned_weights.keys()),
+    "weights": {
+    "labels": list(cleaned_weights.keys()),
             # Convert into percentages instead of proportions
             "data":  [round(x * 100, 2) for x in list(cleaned_weights.values())]
-        },
+            },
 
-        "performance": {
+            "performance": {
             "labels": list(backtest_results_dict.keys()),
             "data": list(backtest_results_dict.values())
-        }
-    }
+            }
+            }
 
     return output
 
@@ -66,7 +66,7 @@ def min_volatility(parameters):
     print("From optimizer: ", parameters)
 
     stocks_df = get_data(parameters["ticker_list"], datetime.strptime(parameters["start_date"], '%Y-%m-%d'),
-                         datetime.strptime(parameters["end_date"], '%Y-%m-%d'))
+       datetime.strptime(parameters["end_date"], '%Y-%m-%d'))
 
     print(stocks_df.head())
 
@@ -80,7 +80,7 @@ def min_volatility(parameters):
 
     cleaned_weights = ef.clean_weights()
 
-    # cleaned_weights = {k: round(v * 100, 3) for k, v in cleaned_weights.items() if v != 0}
+# cleaned_weights = {k: round(v * 100, 3) for k, v in cleaned_weights.items() if v != 0}
     cleaned_weights = {k: v for k, v in cleaned_weights.items() if v != 0}
 
     print(cleaned_weights)
@@ -88,7 +88,7 @@ def min_volatility(parameters):
     # backtest_results = get_daily_returns(cleaned_weights, parameters["start_date"], parameters["end_date"])
 
     backtest_results = backtest_portfolio(prices_df=stocks_df, portfolio=cleaned_weights, initial_amount=int(parameters[
-                                          "initial_amount"]), start_date=parameters["start_date"], end_date=parameters["end_date"])
+      "initial_amount"]), start_date=parameters["start_date"], end_date=parameters["end_date"])
 
     print(backtest_results.describe())
 
@@ -96,17 +96,17 @@ def min_volatility(parameters):
 
     output = {
 
-        "weights": {
-            "labels": list(cleaned_weights.keys()),
+    "weights": {
+    "labels": list(cleaned_weights.keys()),
             # Convert into percentages instead of proportions
             "data":  [round(x * 100, 2) for x in list(cleaned_weights.values())]
-        },
+            },
 
-        "performance": {
+            "performance": {
             "labels": list(backtest_results_dict.keys()),
             "data": list(backtest_results_dict.values())
-        }
-    }
+            }
+            }
 
     return output
 
@@ -115,7 +115,7 @@ def min_volatility_target(parameters):
     print("From optimizer: ", parameters)
 
     stocks_df = get_data(parameters["ticker_list"], datetime.strptime(parameters["start_date"], '%Y-%m-%d'),
-                         datetime.strptime(parameters["end_date"], '%Y-%m-%d'))
+       datetime.strptime(parameters["end_date"], '%Y-%m-%d'))
 
     print(stocks_df.head())
 
@@ -137,7 +137,7 @@ def min_volatility_target(parameters):
     # backtest_results = get_daily_returns(cleaned_weights, parameters["start_date"], parameters["end_date"])
 
     backtest_results = backtest_portfolio(prices_df=stocks_df, portfolio=cleaned_weights, initial_amount=int(parameters[
-                                          "initial_amount"]), start_date=parameters["start_date"], end_date=parameters["end_date"])
+      "initial_amount"]), start_date=parameters["start_date"], end_date=parameters["end_date"])
 
     print(backtest_results.describe())
 
@@ -145,17 +145,17 @@ def min_volatility_target(parameters):
 
     output = {
 
-        "weights": {
-            "labels": list(cleaned_weights.keys()),
+    "weights": {
+    "labels": list(cleaned_weights.keys()),
             # Convert into percentages instead of proportions
             "data":  [round(x * 100, 2) for x in list(cleaned_weights.values())]
-        },
+            },
 
-        "performance": {
+            "performance": {
             "labels": list(backtest_results_dict.keys()),
             "data": list(backtest_results_dict.values())
-        }
-    }
+            }
+            }
 
     return output
 
@@ -164,7 +164,7 @@ def max_return_target(parameters):
     print("From optimizer: ", parameters)
 
     stocks_df = get_data(parameters["ticker_list"], datetime.strptime(parameters["start_date"], '%Y-%m-%d'),
-                         datetime.strptime(parameters["end_date"], '%Y-%m-%d'))
+       datetime.strptime(parameters["end_date"], '%Y-%m-%d'))
 
     print(stocks_df.head())
 
@@ -186,7 +186,7 @@ def max_return_target(parameters):
     # backtest_results = get_daily_returns(cleaned_weights, parameters["start_date"], parameters["end_date"])
 
     backtest_results = backtest_portfolio(prices_df=stocks_df, portfolio=cleaned_weights, initial_amount=int(parameters[
-                                          "initial_amount"]), start_date=parameters["start_date"], end_date=parameters["end_date"])
+      "initial_amount"]), start_date=parameters["start_date"], end_date=parameters["end_date"])
 
     print(backtest_results.describe())
 
@@ -194,23 +194,24 @@ def max_return_target(parameters):
 
     output = {
 
-        "weights": {
-            "labels": list(cleaned_weights.keys()),
+    "weights": {
+    "labels": list(cleaned_weights.keys()),
             # Convert into percentages instead of proportions
             "data":  [round(x * 100, 2) for x in list(cleaned_weights.values())]
-        },
+            },
 
-        "performance": {
+            "performance": {
             "labels": list(backtest_results_dict.keys()),
             "data": list(backtest_results_dict.values())
-        }
-    }
+            }
+            }
 
     return output
 
 def target_return_volatility(parameters):
-    """Basic optimization function for the basic interface.
     
+    """Basic optimization function for the basic interface.
+
     Function tries to match a portfolio volatility and return.
     
     Arguments:
@@ -223,7 +224,7 @@ def target_return_volatility(parameters):
     print("From optimizer: ", parameters)
 
     stocks_df = get_all_snp500_data(datetime.strptime(parameters["start_date"], '%Y-%m-%d'),
-                         datetime.strptime(parameters["end_date"], '%Y-%m-%d'))
+       datetime.strptime(parameters["end_date"], '%Y-%m-%d'))
     print("Stocks df")
     print(stocks_df.head())
 
@@ -245,8 +246,7 @@ def target_return_volatility(parameters):
 
     # backtest_results = get_daily_returns(cleaned_weights, parameters["start_date"], parameters["end_date"])
 
-    backtest_results = backtest_portfolio(prices_df=stocks_df, portfolio=cleaned_weights, initial_amount=int(parameters[
-                                          "initial_amount"]), start_date=parameters["start_date"], end_date=parameters["end_date"])
+    backtest_results = backtest_portfolio(prices_df=stocks_df, portfolio=cleaned_weights, initial_amount=1000, start_date=parameters["start_date"], end_date=parameters["end_date"])
 
     print(backtest_results.describe())
 
@@ -254,17 +254,17 @@ def target_return_volatility(parameters):
 
     output = {
 
-        "weights": {
-            "labels": list(cleaned_weights.keys()),
+    "weights": {
+    "labels": list(cleaned_weights.keys()),
             # Convert into percentages instead of proportions
             "data":  [round(x * 100, 2) for x in list(cleaned_weights.values())]
-        },
+            },
 
-        "performance": {
+            "performance": {
             "labels": list(backtest_results_dict.keys()),
             "data": list(backtest_results_dict.values())
-        }
-    }
+            }
+            }
 
     return output
 
